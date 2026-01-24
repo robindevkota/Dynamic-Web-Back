4b02e730178dfad508da833dae7ea127a960e1be
Robin Devkota <robin.devkota@amniltech.com>
2026-01-25 00:44:48 +0545

    fix:licience

--- controllers/aiGeneratorController.js
+++ controllers/aiGeneratorController.js
@@ -1,3 +1,9 @@
+ * @file aiGeneratorController.js
+  * @copyright(c) 2026 Robin Devkota.All Rights Reserved.
+ * @license Proprietary - No unauthorized copying or distribution.
+ * For inquiries, contact: robindevkta0 @gmail.com
+ */
+
 // backend/controllers/aiGeneratorController.js
 // Updated 2026 – more reliable Ollama JSON generation
 
